9946a7eacfe17484f547befe8472fdbc315be31f
Sylbila <sylbilanajla@gmail.com>
2024-12-11 09:40:32 +0800

    add login, logout, and store map

---
+++ app/src/main/java/com/example/freshguard/data/response/LoginResponse.kt
@@ -0,0 +1,42 @@
+package com.example.freshguard.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class LoginResponse(
+
+    @field:SerializedName("data")
+    val data: DataLogin,
+
+    @field:SerializedName("message")
+    val message: String,
+
+    @field:SerializedName("status")
+    val status: String
+)
+
+data class UserLogin(
+
+    @field:SerializedName("name")
+    val name: String? = null,
+
+    @field:SerializedName("created_at")
+    val createdAt: String? = null,
+
+    @field:SerializedName("id")
+    val id: String? = null,
+
+    @field:SerializedName("username")
+    val username: String? = null
+)
+
+data class DataLogin(
+
+    @field:SerializedName("token_expires_at")
+    val tokenExpiresAt: Any? = null,
+
+    @field:SerializedName("user")
+    val user: UserLogin? = null,
+
+    @field:SerializedName("token")
+    val token: String
+)
